5a5caff46dcf7aaebe0f4a6e55328028d080cdfe
eak netpanya <eak.netpanya@gmail.com>
2024-05-29 10:55:42 +0700

    init docker compose

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # minimal-jupyter-notebook-stack
 Minimal Jupyter Notebook Stack
+
+
+https://hub.docker.com/r/jupyter/minimal-notebook
---
+++ jupyter_server_config.py
@@ -0,0 +1,5 @@
+c.ServerApp.allow_remote_access = True
+c.ServerApp.allow_root = True
+c.ServerApp.base_url = ''
+c.ServerApp.ip = '*'
+c.ServerApp.token = ''
